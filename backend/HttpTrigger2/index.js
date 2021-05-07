95c23f5397360468ede897ef71e7c5c29aa23d4c
Radossus <spool@seznam.cz>
2021-05-07 12:04:20 +0200

    add

---
+++ backend/HttpTrigger2/index.js
@@ -0,0 +1,9 @@
+module.exports = async function (context, req, data) {
+    console.log("Function has been triggered - Azure");
+    context.bindings.outputDocument = data[0];
+    context.bindings.outputDocument.counter += 1;
+    context.res = {
+       body: data[0].counter
+  
+    };
+  }
